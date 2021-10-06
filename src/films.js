6cefc24458c4d4b4753643f919d99c8068918213
Krystel Rodriguez Castejon <krystel.dev@gmail.com>
2021-10-06 09:29:25 +0200

    Nivell3

--- src/films.js
+++ src/films.js
@@ -29,8 +29,8 @@ function moviesAverageOfDirector(array, director) {
   let sumatori = peliculasDirector.reduce((total, pelicula) => total + pelicula.score, 0);
 
   //Buscar la mitja
-  let result = Math.round((sumatori/qDirector)*100)/100;
-  
+  let result = Math.round((sumatori / qDirector) * 100) / 100;
+
   console.log("EXERCICE 3 ->", result);
   return result;
 }
@@ -38,16 +38,16 @@ function moviesAverageOfDirector(array, director) {
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
   let arraynew = [...array]; // Operador spread (de propagación) en Javascript ES6
-   let result = arraynew.sort(function (a, b) {
-     const titleA = a.title;
-     const titleB = b.title;
-     if (titleA < titleB) {
-       return -1;
-     }
-     if (titleA > titleB) {
-       return 1;
-     }
-     return 0;
+  let result = arraynew.sort(function (a, b) {
+    const titleA = a.title;
+    const titleB = b.title;
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1;
+    }
+    return 0;
   });
   result = result.map(pelicula => {
     return pelicula.title
@@ -90,8 +90,8 @@ function orderByYear(array) {
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory(array, categoria) {
   //Buscar un listado de las peliculas deseadas:
-  let moviesSameCategory = array.filter(movieSameCategory => ((movieSameCategory.genre == categoria) && (movieSameCategory.score!=="")) );
-  
+  let moviesSameCategory = array.filter(movieSameCategory => ((movieSameCategory.genre == categoria) && (movieSameCategory.score !== "")));
+
 
   //Buscar la cantidad de peliculas deseadas:
   let qMoviesSameCategory = moviesSameCategory.length;
@@ -100,22 +100,77 @@ function moviesAverageByCategory(array, categoria) {
   let sumatorio = moviesSameCategory.reduce((total, movie) => total + movie.score, 0);
 
   //Buscar la valoración de la categoria:
-  let result = Math.round((sumatorio/qMoviesSameCategory)*100)/100;
-  
-  console.log(moviesSameCategory);
-  console.log(sumatorio);
-  console.log("EXERCICE 6 ->", result);
+  let result = Math.round((sumatorio / qMoviesSameCategory) * 100) / 100;
+
+  console.log("EXERCICE 6 ->" + result);
   return result;
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
+function hoursToMinutes(array) {
+  //Array nova:
+  let clonedArray = JSON.parse(JSON.stringify(array));
+  //[...array]; -> No funciona.
+
+  //Recorre tots els elements de la array
+  let result = clonedArray.map(tranformarAMinuts);
+
+  console.log("EXERCICE 7 ->", result);
+  return result;
 
 }
 
+function tranformarAMinuts(pelicula) {
+  pelicula.durationSplit = pelicula.duration.split(" ");
+  if (pelicula.durationSplit.length == 2) {
+    pelicula.durationSplit[0] = (parseInt(pelicula.durationSplit[0].replace(/\D/g, ''), 10));
+    pelicula.durationSplit[0] = pelicula.durationSplit[0] * 60;
+    pelicula.durationSplit[1] = (parseInt(pelicula.durationSplit[1].replace(/\D/g, ''), 10));
+    pelicula.duration = pelicula.durationSplit[0] + pelicula.durationSplit[1];
+  } else {
+    if (pelicula.durationSplit[0].includes('h')) {
+      pelicula.durationSplit[0] = (parseInt(pelicula.durationSplit[0].replace(/\D/g, ''), 10));
+      pelicula.durationSplit[0] = pelicula.durationSplit[0] * 60;
+      pelicula.duration = pelicula.durationSplit[0];
+    } else {
+      pelicula.durationSplit[0] = (parseInt(pelicula.durationSplit[0].replace(/\D/g, ''), 10));
+      pelicula.duration = pelicula.durationSplit[0];
+    }
+  }
+  delete pelicula.durationSplit;
+
+  return pelicula;
+}
+
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
+function bestFilmOfYear(array, any) {
+  let arraynew= JSON.parse(JSON.stringify(array));
+
+  //Buscar totes les pelicules del any X
+  let peliculasAny = arraynew.filter(pelicula => pelicula.year == any);
+
+  //Ordenar de score mayor a inferior:
+  let scoreOrdenado = peliculasAny.sort(function (a, b) {
+    const scoreA = a.score;
+    const scoreB = b.score;
+    if (scoreA < scoreB) {
+      return 1;
+    }
+    if (scoreA > scoreB) {
+      return -1;
+    }
+  });
+
+  //Saber el valor del score máximo:
+  let scoreMaximo = scoreOrdenado[0].score;
   
+  //Buscar a que pelicula pertenece el scoremaximo:
+  let result = scoreOrdenado.filter( element => element.score == scoreMaximo);
+
+  console.log("any", any);
+  console.log("scoreMaximo", scoreMaximo);
+  console.log("EXERCICE 8 ->", result);
+  return result;
 }
 
 
@@ -132,5 +187,5 @@ if (typeof module !== 'undefined') {
     moviesAverageByCategory,
     hoursToMinutes,
     bestFilmOfYear,
-  };
+  }
 }
